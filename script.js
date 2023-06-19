9cdbc343c839e1671d2019e3d505ae07a7db321e
dopalescent <skudlgre000@gmail.com>
2023-06-18 22:35:52 -0400

    removed console.logs, removed sandbox from bottom of js sheet, cleaned up js

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var specChars = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '{', '}', '[', ']', '|', '/', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '?']
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specChars = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '{', '}', '[', ']', '|', '/', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '?'];
 
 // Write password to the #password input
 function writePassword() {
-  var chars = []
+  var chars = [];
   var newPassword = [];
-  var check
+  var check;
 
   function checker() {
     var test = false;
     for (var j = 0; j < check.length; j++) {
       if (newPassword.includes(check[j])) {
-        console.log(check[j])
         test = true;
-        console.log(test)
       };
-    }
-    console.log(test)
+    };
     if (!test) {
-      console.log("test fail");
       generatePassword();
       return;
-    } else {
-      console.log("test pass");
-    }
+    };
   }
-
 
   var passLength = window.prompt("Choose password length: enter a numeric value between 8 and 128.");
   if (isNaN(passLength)) {
@@ -41,25 +34,22 @@ function writePassword() {
     window.alert("Error: Password length must be between 8 and 128.");
     return;
   }
+  
   var askLower = window.confirm("Use lowercase letters?");
   if (askLower) {
     chars = chars.concat(lower);
-    console.log(chars);
   }
   var askUpper = window.confirm("Use uppercase letters?");
   if (askUpper) {
     chars = chars.concat(upper);
-    console.log(chars);
   }
   var askNumber = window.confirm("Use numbers?");
   if (askNumber) {
     chars = chars.concat(numbers);
-    console.log(chars);
   }
   var askSpec = window.confirm("Use special characters?");
   if (askSpec) {
     chars = chars.concat(specChars);
-    console.log(chars);
   }
   if (!askLower && !askUpper && !askNumber && !askSpec) {
     window.alert("Error: At least one character type must be selected.");
@@ -72,28 +62,22 @@ function writePassword() {
     for (var i = 0; i < passLength; i++) {
       newPassword[i] = chars[Math.floor(Math.random() * chars.length)];
     };
-    console.log(newPassword.join(''));
     if (askLower) {
-      console.log("lower ask registered")
       check = lower;
       checker();
     };
     if (askUpper) {
-      console.log("upper ask registered")
       check = upper;
       checker();
     };
     if (askNumber) {
-      console.log("number ask registered")
       check = numbers;
       checker();
     };
     if (askSpec) {
-      console.log("spec ask registered")
       check = specChars;
       checker();
     };
-
     return newPassword.join('');
   };
 
@@ -103,58 +87,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// lower.unshift("A");
-// console.log(lower);
-// for (var j = 0; j < lower.length; j++) {
-//   lower.some(upper.includes(j));
-// }
-// console.log(lower.includes("A"))
-
-// for (var j = 0; j < upper.length; j++) {
-//   if (lower.includes(upper[j])) {
-//     console.log(upper[j])
-//   } else {
-//     console.log("none found")
-//   }
-// }
-
-
-// function checkUpper() {
-//   var test = false;
-//   for (var j = 0; j < upper.length; j++) {
-//     if (lower.includes(upper[j])) {
-//       console.log(upper[j])
-//       test = true;
-//       console.log(test)
-//     };
-//   }
-//   console.log(test)
-//   if (!test) {
-//     console.log("test fail")
-//   } else {
-//     console.log("test pass")
-//   }
-// }
-// checkUpper();
-
-// function checker() {
-//   var test = false;
-//   var check = upper;
-//   check = specChars;
-// lower.unshift("#");
-//   for (var j = 0; j < check.length; j++) {
-//     if (lower.includes(check[j])) {
-//       console.log(check[j])
-//       test = true;
-//       console.log(test)
-//     };
-//   }
-//   console.log(test)
-//   if (!test) {
-//     console.log("test fail")
-//   } else {
-//     console.log("test pass")
-//   }
-// }
-// checker();
