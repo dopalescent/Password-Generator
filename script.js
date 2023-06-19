372626abc253901d27a3c5bbb39d77381c554b6a
dopalescent <skudlgre000@gmail.com>
2023-06-18 23:08:43 -0400

    added notation to js sheet, added bojangles conditional

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-// Assignment Code
+// Starter code for "Generate Password" button
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
+// Character type arrays
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specChars = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '{', '}', '[', ']', '|', '/', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '?'];
 
-// Write password to the #password input
+// Function initiated by clicking "Generate Password" button
 function writePassword() {
   var chars = [];
   var newPassword = [];
   var check;
 
+  // Function for verifying character type inclusion in generated password, called in generatePassword function at line 69
   function checker() {
     var test = false;
     for (var j = 0; j < check.length; j++) {
@@ -25,7 +28,12 @@ function writePassword() {
     };
   }
 
+  // Password length prompt
   var passLength = window.prompt("Choose password length: enter a numeric value between 8 and 128.");
+  if (passLength.toLowerCase() === "bojangles"){
+    window.alert("*************  YOU CAN'T SAY 'BOJANGLES'!!!  *************");
+    return;
+  }
   if (isNaN(passLength)) {
     window.alert("Error: Password length must be entered as a numeric value.");
     return;
@@ -35,6 +43,7 @@ function writePassword() {
     return;
   }
   
+  // Character type confirm windows
   var askLower = window.confirm("Use lowercase letters?");
   if (askLower) {
     chars = chars.concat(lower);
@@ -56,8 +65,7 @@ function writePassword() {
     return;
   }
 
-  var password = generatePassword();
-
+  // Password generation function
   function generatePassword () {
     for (var i = 0; i < passLength; i++) {
       newPassword[i] = chars[Math.floor(Math.random() * chars.length)];
@@ -81,9 +89,8 @@ function writePassword() {
     return newPassword.join('');
   };
 
+  // Starter code for displaying generated password in the browser
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
